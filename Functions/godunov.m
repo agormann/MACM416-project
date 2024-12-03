27249042dbfb93081911e3d60ba4cb5d96342838
agormann <ajgormann@gmail.com>
2024-12-03 07:11:54 -0800

    adding changes

---
+++ Functions/godunov.m
@@ -0,0 +1,40 @@
+function [T,U] = godunov(X,u0,tf)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+f = @(u) 0.5*u.^2; % flux function
+nx = length(X);
+dx = abs(X(1)-X(2));
+u = u0;
+t = 0;
+U = u;
+T = t;
+
+while t < tf
+    dt = dx/max(abs(u)); % compute dt (dynamic)
+
+    flux = f(u); % initial flux
+    F = zeros(size(u)); % flux update
+
+    % solving riemann problem exactly
+    for i = 1:nx
+        uL = u(i);
+        fuL = flux(i);
+        uR = u(mod(i,nx)+1);
+        fuR = flux(mod(i,nx)+1);
+        if uL <= uR
+            F(i) = min(fuL,fuR);
+        elseif uL > uR
+            F(i) = max(fuL,fuR);
+        else
+            error('OOPSIE WOOPSIE!!')
+        end
+    end
+    
+    u = u - dt/dx*(F - circshift(F,1)); % conservative update
+    t = t + dt;
+    U = [U; u];
+    T = [T; t];
+end
+
+end
---
+++ Functions/riemann.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = riemann(inputArg1,inputArg2)
+%RIEMANN Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
